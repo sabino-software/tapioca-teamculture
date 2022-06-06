54f6bdb7d7f68ad69be39f3c3ef100fdeb7c7d37
Felipe Guilherme Sabino <felipe@sabino.pro>
2022-06-05 23:24:33 -0300

    feat: additional resources

--- tapioca_teamculture/resource_mapping.py
+++ tapioca_teamculture/resource_mapping.py
@@ -1,17 +1,53 @@
 # coding: utf-8
 
 RESOURCE_MAPPING = {
+    'attributes': {
+        'resource': 'attributes',
+        'methods': ['GET', 'POST']
+    },
+    'attribute': {
+        'resource': 'attributes/{id}',
+        'methods': ['GET', 'PUT']
+    },
     'groups': {
         'resource': 'groups',
-        'methods': ['GET', 'POST']
+        'methods': ['GET']
+    },
+    'group_create': {
+        'resource': 'v2/groups',
+        'methods': ['POST']
+    },
+    'group_inactivate': {
+        'resource': 'groups/{id}/inactivation',
+        'methods': ['POST']
+    },
+    'group_activate': {
+        'resource': 'groups/{id}/activation',
+        'methods': ['POST']
     },
     'tree': {
         'resource': 'groups/tree',
         'methods': ['GET']
     },
+    'user': {
+        'resource': 'users/{email}',
+        'methods': ['GET', 'DELETE']
+    },
     'users': {
         'resource': 'users',
         'methods': ['GET', 'POST']
+    },
+    'user_create': {
+        'resource': 'v2/users',
+        'methods': ['POST']
+    },
+    'user_reactivate': {
+        'resource': 'users/{email}/reactivate',
+        'methods': ['PUT']
+    },
+    'user_undo_inactivation': {
+        'resource': 'users/{email}/undo-inactivate',
+        'methods': ['PUT']
     },
     'action_plans': {
         'resource': 'action-plans',
